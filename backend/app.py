65a9bf53efb25ac4725e97c8be9ace8337b34574
Sandy_undefined <08sandysk@gmail.com>
2021-06-08 19:23:55 +0530

    Basic setup

---
+++ backend/app.py
@@ -0,0 +1,40 @@
+# from flask import Flask, render_template, request
+# from chatterbot import ChatBot
+# from chatterbot.trainers import ChatterBotCorpusTrainer
+# from chatterbot.trainers import ListTrainer
+
+# app = Flask(__name__)
+# bot = ChatBot("Sunanda's Resume ChatBot")
+# trainer = ListTrainer(bot)
+
+# file =  open("static/file.text", "r")
+# conversation = file.read()
+
+# trainer.train(conversation)
+
+# @app.route("/")
+# def home():
+#     return render_template("index.html")
+
+# @app.route("/")
+# def get_bot_response():
+#     userText = request.args.get('msg')
+#     print("working")
+#     return str(bot.get_response(userText))
+
+# if __name__ == "__main__":
+#     app.run()
+
+from flask import Flask, jsonify,request
+import time
+
+app = Flask(__name__)
+
+@app.route("/bot", methods=["POST"])
+def response():
+    query = dict(request.form)['query']
+    res = query + " " + time.ctime()
+    return jsonify({"response" : res})
+    
+if __name__=="__main__":
+    app.run(host="0.0.0.0",)
