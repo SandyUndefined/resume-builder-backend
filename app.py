9beaf3abfe5988596236a02163bb6eae0fd72d3c
Sandy_undefined <08sandysk@gmail.com>
2022-03-09 02:06:50 +0530

    updated project

--- app.py
+++ app.py
@@ -1,40 +1,43 @@
-# from flask import Flask, render_template, request
-# from chatterbot import ChatBot
-# from chatterbot.trainers import ChatterBotCorpusTrainer
-# from chatterbot.trainers import ListTrainer
+from flask import Flask, render_template, request
+from flask import Flask, jsonify,request
+from chatterbot import ChatBot
+from chatterbot.trainers import ChatterBotCorpusTrainer
+from chatterbot.trainers import ListTrainer
 
-# app = Flask(__name__)
-# bot = ChatBot("Sunanda's Resume ChatBot")
-# trainer = ListTrainer(bot)
+app = Flask(__name__)
+bot = ChatBot("Flutter")
+trainer = ListTrainer(bot)
 
-# file =  open("static/file.text", "r")
-# conversation = file.read()
+file =  open("static/file.text", "r")
+conversation = file.read()
 
-# trainer.train(conversation)
+trainer.train(conversation)
 
 # @app.route("/")
 # def home():
 #     return render_template("index.html")
 
-# @app.route("/")
-# def get_bot_response():
-#     userText = request.args.get('msg')
-#     print("working")
-#     return str(bot.get_response(userText))
+@app.route("/", methods = ['GET','POST'])
+def get_bot_response():
+    if request.method == 'GET':
+        userText = request.args.get('msg')
+        print("working", userText)
+        res = str(bot.get_response(userText))
+        return jsonify({"response" : res})
+    elif request.method == 'POST':
+        print("POST")
 
-# if __name__ == "__main__":
-#     app.run()
-
-from flask import Flask, jsonify,request
-import time
-
-app = Flask(__name__)
-
-@app.route("/bot", methods=["POST"])
-def response():
-    query = dict(request.form)['query']
-    res = query + " " + time.ctime()
-    return jsonify({"response" : res})
-    
-if __name__=="__main__":
+if __name__ == "__main__":
     app.run()
+
+
+# app = Flask(__name__)
+
+# @app.route("/bot", methods=["POST"])
+# def response():
+#     query = dict(request.form)['query']
+#     res = query + " " + time.ctime()
+#     return jsonify({"response" : res})
+    
+# if __name__=="__main__":
+#     app.run()
